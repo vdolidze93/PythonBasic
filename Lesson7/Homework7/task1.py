3f41666ff0ee0bfef9554ae734954cdbe179f6cb
vdolidze93 <dolia1903@gmail.com>
2025-06-13 17:58:31 +0200

    first task - write a first function realization

---
+++ Lesson7/Homework7/task1.py
@@ -0,0 +1,23 @@
+# Написати функцію say_hi, яка представить людину за переданими параметрами.
+#
+# Вхідні дані: Два аргументи рядок(str) та позитивне число(int)
+#
+# Функція має повернути рядок.
+#
+# Замініть pass на Ваше рішення.
+
+# Copy code
+# def say_hi(name, age):
+#   pass
+#
+# assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+# assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+# print('ОК')
+
+
+def say_hi(name, age):
+    return f'Hi. My name is {name} and I\'m {age} years old'
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
